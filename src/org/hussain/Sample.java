a670ff209265d02b87ff2d6f53026983b1515dda
Hussain <hussenvalli.k@gmail.com>
2021-06-01 16:45:13 +0530

    added setters and getters

--- src/org/hussain/Sample.java
+++ src/org/hussain/Sample.java
@@ -2,6 +2,17 @@ package org.hussain;
 
 public class Sample {
    private  String str;
+   
+
+public String getStr() {
+    return str;
+}
+
+
+public void setStr(String str) {
+    this.str = str;
+}
+
 
 @Override
 public String toString() {
